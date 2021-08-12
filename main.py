92cd762d0d99e5e28f3819a6bdee9a311b7653b2
JDiegoS <46642689+JDiegoS@users.noreply.github.com>
2021-08-12 01:04:29 -0600

    Sign in, menu y mostrar roster

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import sys
 import asyncio
 import logging
 from getpass import getpass
-from slixmpp.exceptions import IqError, IqTimeout
+from slixmpp.xmlstream.asyncio import asyncio
 
 import slixmpp
 
@@ -14,15 +14,66 @@ import slixmpp
 class Client(slixmpp.ClientXMPP):
     def __init__(self, jid, password):
         super().__init__(jid, password)
-
         self.add_event_handler('session_start', self.start)
 
         #Al recibir mensaje
-        self.add_event_handler('message', self.message)
-    
-    def start(self, event):
+        #self.add_event_handler('message', self.message)
+
+    async def getUsers(self):
+        try:
+            self.get_roster()
+        except:
+            print("Error al obtener roster")
+        
+        self.send_presence()
+        print('Waiting for presence updates...\n')
+        #self.presences_received.wait(5)
+        await asyncio.sleep(5)
+        print("\nLista de usuarios:")
+        
+        groups = self.client_roster.groups()
+        for group in groups:
+            for jid in groups[group]:
+                sub = self.client_roster[jid]['subscription']
+                name = self.client_roster[jid]['name']
+                if self.client_roster[jid]['name']:
+                    print(' %s (%s) [%s]' % (name, jid, sub))
+                else:
+                    print(' %s [%s]' % (jid, sub))
+
+                connections = self.client_roster.presence(jid)
+                for res, pres in connections.items():
+                    show = 'available'
+                    if pres['show']:
+                        show = pres['show']
+                    print('   - %s (%s)' % (res, show))
+                    if pres['status']:
+                        print('       %s' % pres['status'])
+    async def start(self, event):
         self.send_presence()
         self.get_roster()
+
+        print("\nBienvenido al programa " + self.jid)
+        sigue = True
+        while sigue == True:
+            opc2 =  int(input("\nIngrese una opcion:\n1. Mostrar Usuarios \n2. Agregar Contacto \n3. Mostrar Usuario \n4. Mensaje de Presencia \n5. Mensaje Privado \n6. Mensaje Grupal\n7. Log out \n"))
+            if opc2 == 1:
+                #Mostrar Usuarios
+                await self.getUsers()
+            elif opc2 == 2:
+                pass
+            elif opc2 == 3:
+                pass
+            elif opc2 == 4:
+                pass
+            elif opc2 == 5:
+                pass
+            elif opc2 == 6:
+                pass
+            elif opc2 == 7:
+                print("Hasta luego!")
+                self.disconnect()
+                sigue = False
     
     def message(self, msg):
         if msg['type'] in ('normal', 'chat'):
@@ -53,8 +104,6 @@ class Register(slixmpp.ClientXMPP):
             print("Error al registrarse")
             self.disconnect()
 
-def mostrarUsuarios():
-    pass
 
 def agregarContacto():
     pass
@@ -73,9 +122,9 @@ def chatGrupal():
 
 
 termino = False
-print("Bienvenido \n")
+print("\nBienvenido")
 while termino != True:
-    opc1 = int(input("Ingrese una opcion:\n1. Iniciar sesion \n2. Registrar nuevo usuario \n3. Salir\n"))
+    opc1 = int(input("\nIngrese una opcion:\n1. Iniciar sesion \n2. Registrar nuevo usuario \n3. Salir\n"))
     if opc1 == 1:
         #Iniciar sesion
         userName = input("Ingrese el usuario: ")
@@ -83,7 +132,7 @@ while termino != True:
 
         currentC = Client(userName, password)
         currentC.connect()
-        currentC.process()
+        currentC.process(forever=False)
 
     elif opc1 == 2:
         #Registrar
